dfc3d855405a278915f436c33ec8ecc7e5fddb54
root <root@localhost.localdomain>
2019-05-05 07:19:27 +0100

    add build db

---
+++ Build/build_db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+projectName="DBServer"
+testpath="../"$projectName
+
+cd $testpath
+
+echo -en "\e[1;32mstart build $projectName : \e[0m"
+
+go build -o $projectName main.go
+
+ret=$?
+if [ $ret -eq 0 ];then
+	echo -e "\e[1;32m 【ok】\e[0m"
+else
+	echo -e "\e[1;32m 【failed】\e[0m"
+fi
+
