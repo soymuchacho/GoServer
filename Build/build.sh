568b7107a32ab18345695b1ec0c78992eda242b1
soymuchacho <guqi_282@126.com>
2019-05-09 15:52:18 +0800

    modify build

--- Build/build.sh
+++ Build/build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+PROJECT_NAME="GoServer"
+
+Archive(){
+	dir=../${PROJECT_NAME}-${BUILD_VERSION}
+	mkdir -p $dir
+	mkdir -p $dir/AgentServer
+	mkdir -p $dir/DBServer
+	mkdir -p $dir/AgentServer/conf
+	mkdir -p $dir/DBServer/conf
+
+	cp -rt $dir ../goserver.service
+	cp -rt $dir ../install.sh
+	cp -rt $dir ../runserv.sh
+	cp -rt $dir/AgentServer ../AgentServer/AgentServer
+	cp -rt $dir/AgentServer ../AgentServer/conf
+	cp -rt $dir/DBServer ../DBServer/DBServer
+	cp -rt $dir/DBServer ../DBServer/conf
+	tar -czf ${PROJECT_NAME}-${BUILD_VERSION}.tar $dir
+
+	mv ${PROJECT_NAME}-${BUILD_VERSION}.tar ../
+}
+
+
 case "$1" in
 	proto)
 		./build_proto.sh
@@ -22,6 +45,7 @@ case "$1" in
 		./build_convert.sh
 		./build_db.sh
 		./build_test.sh
+		Archive
 		;;
 	*)
 		echo "Usage: build.sh {proto|agent|convert|db|test|all}"
